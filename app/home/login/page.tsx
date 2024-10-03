680d1a5d89fd3a4e20c391e8f3c14a6cc0199527
ashkesperanza <134548062+ashkesperanza@users.noreply.github.com>
2024-10-02 19:18:19 -0700

    Create page.tsx

---
+++ app/home/login/page.tsx
@@ -0,0 +1,13 @@
+import { poppins } from "@/public/font";
+
+export default function Login() {
+    return (
+        <div className={`flex items-center justify-center min-h-screen ${poppins.className}`}>
+            <div className="flex items-center justify-center shadow-lg h-96 w-1/4">
+                <form>
+                   
+                </form>
+            </div>
+        </div>
+    );
+}
